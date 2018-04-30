f07b3d4e1ec1ba63964a5085011b5aa20191ef79
thiagofrancisquete <thiago.francisquete@gmail.com>
2018-04-30 20:28:56 -0300

    fix mongoose conection error

--- server.js
+++ server.js
@@ -1,31 +1,50 @@
-//chamando os pacotes
+/* chamando os pacotes */
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var produto = require('./app/models/produto');
 
-// conectando o mongoose
-mongoose.connect('mongodb://<crud-api-restful>:<thiago123>@ds014648.mlab.com:14648/node-crud-api');
+/* conectando o mlab (cloud) */
+mongoose.connect('mongodb://crud-api-restful:thiago123@ds014648.mlab.com:14648/node-crud-api');
 
+/* conectando local
+mongoose.connect('mongodb://localhost/nome-da-api)
+*/
 
-//configuracao da variavel app para o body-parser
-app.use(bodyParser.urlencoded({ extended: true}));
+/* configuracao da variavel app para o body-parser */
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(bodyParser.json());
 
-// definindo uma porta para executar a API
+/* definindo uma porta para executar a API */
 var port = process.env.port || 8000;
 
+/* Rotas da API */
 // criando uma instancia das rotas via express
 var router = express.Router();
 
-// criando uma rota de exemplo
-router.get('/', function(req, res){
-  res.json({ message: 'Beleza! Bem vindo a nossa loja!'});
+
+router.use(function(req, res, next){
+  console.log("Algo esta acontecendo aqui...");
+  next();
 });
 
-// definindo um padrao das rotas prefixadas com '/api'
+/* criando uma rota de exemplo */
+router.get('/', function (req, res) {
+  res.json({
+    message: 'Beleza! Bem vindo a nossa loja!'
+  });
+});
+
+/* APIS */
+
+
+
+/* definindo um padrao das rotas prefixadas com '/api' */
 app.use('/api', router);
 
-// iniciando a aplicação
+/* iniciando a aplicação */
 app.listen(port);
 console.log('iniciando a app na porta ' + port);
