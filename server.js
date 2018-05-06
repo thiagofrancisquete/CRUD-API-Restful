736017c263d7037c120c207cb1912ce619d36b96
thiagofrancisquete <thiago.francisquete@gmail.com>
2018-05-06 03:43:48 -0300

    criado metodo delete

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ var port = process.env.port || 8000;
 var router = express.Router();
 
 
-router.use(function(req, res, next){
+router.use(function (req, res, next) {
   console.log("Algo esta acontecendo aqui...");
   next();
 });
@@ -43,61 +43,79 @@ router.get('/', function (req, res) {
 /* APIS */
 
 // metodo POST
-router.route('/produtos').post(function(req, res){
-  var produto = new Produto();
+router.route('/produtos').post(function (req, res) {
+    var produto = new Produto();
 
-  produto.nome = req.body.nome;
-  produto.preco = req.body.preco;
-  produto.desc = req.body.desc;
+    produto.nome = req.body.nome;
+    produto.preco = req.body.preco;
+    produto.desc = req.body.desc;
 
-  produto.save(function(error){
-    if(error)
-      res.send('Erro ao tentar salvar o produto ' +error);
-    res.json({ message: 'Produto cadastrado com sucesso!'});
+    produto.save(function (error) {
+      if (error)
+        res.send('Erro ao tentar salvar o produto ' + error);
+      res.json({
+        message: 'Produto cadastrado com sucesso!'
+      });
+    })
   })
-})
 
-// selecionar todos os produtos, acessar em GET http://localhost:8000/api/produtos
-  .get(function(req, res){
-    Produto.find(function(error, produtos){
-      if(error)
+  // selecionar todos os produtos, acessar em GET http://localhost:8000/api/produtos
+  .get(function (req, res) {
+    Produto.find(function (error, produtos) {
+      if (error)
         res.send("erro ao tentar selecionar todos os produtos " + error);
       res.json(produtos);
     })
   })
 
- // rotas que irão terminar em '/produtos/produto_id' 
+// rotas que irão terminar em '/produtos/produto_id' 
 router.route('/produtos/:produto_id')
 
-// selecionar por id (http://localhost:8000/api/produtos/produto_id)
-.get(function(req, res){
-  Produto.findById(req.params.produto_id, function(error, produto){
-    if(error)
-      res.send('Id do produto nao encontrado ' +error)
-    res.json(produto)
+  // selecionar por id (http://localhost:8000/api/produtos/produto_id)
+  .get(function (req, res) {
+    Produto.findById(req.params.produto_id, function (error, produto) {
+      if (error)
+        res.send('Id do produto nao encontrado ' + error)
+      res.json(produto)
+    })
   })
-})
 
-// metodo PUT para atualização
-.put(function(req, res){
-  // achar o id do produto
-  produto.findById(req.params.produto_id, function(error, produto){
-    if(error)
-      res.send('Id do produto nao encontrado ' +error)
-      
+  // metodo PUT para atualização
+  .put(function (req, res) {
+    // achar o id do produto
+    produto.findById(req.params.produto_id, function (error, produto) {
+      if (error)
+        res.send('Id do produto nao encontrado ' + error)
+
       // atualizando os dados
       produto.nome = req.body.nome;
       produto.preco = req.body.preco;
       produto.descricao = req.body.descricao;
 
       // salvando as propriedades
-      produto.save(function(error){
-        if(error)
-          res.send('Erro au atualizar o produto ' +error);
-        res.json({ message: 'Produto atualizado com sucesso'})
+      produto.save(function (error) {
+        if (error)
+          res.send('Erro au atualizar o produto ' + error);
+        res.json({
+          message: 'Produto atualizado com sucesso'
+        })
       })
+    })
   })
-})
+
+  // criando metodo delete
+  .delete(function (req, res) {
+
+    Produto.remove({
+      _id: req.params.produto_id
+    }, function (error) {
+      if (error)
+        res.send('Id do produto nao encontrado ' + error);
+      res.json({
+        message: 'Produto excluido com sucesso'
+      });
+    });
+  })
 
 /* definindo um padrao das rotas prefixadas com '/api' */
 app.use('/api', router);
